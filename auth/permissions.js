caeb03d2f581000333c985186255552e323e2415
jimboweb <jimboweb@gmail.com>
2018-08-25 12:13:25 -0400

    category routes replaced callbacks with await

--- auth/permissions.js
+++ auth/permissions.js
@@ -1,3 +1,5 @@
+const Branch = require('../models/Branch');
+
 /**
  * For now this just checks that the user owns the object. In
  * the future this may allow for tasks to be shared wtih other users.
@@ -7,4 +9,27 @@
  */
 exports.checkObjectPermissions=(objAccountId,userAccountId)=>{
     return objAccountId===userAccountId;
+}
+
+exports.runIfOwnershipVerified = async (objType, verbString, id, userId, res, callback)=>{
+    let typeString;
+    if(objType === Branch.Category){
+        typeString = 'category';
+    } else if (objType === Branch.Task){
+        typeString = 'task';
+    } else if (objType === Branch.Event){
+        typeString = 'event'
+    } else {
+        typeString = 'note'
+    }
+    try {
+        if (!(await Branch.verifyOwnership(Branch.Category, id, userId))) {
+            res.status(403).send({"err": `You are not authorized to ${verbString} that ${typeString}`})
+        } else {
+            callback();
+        }
+    } catch (e) {
+        res.status(500).send({'err':`There was an error verifying the ownership of that ${typeString}`})
+    }
+
 }
--- routes/category.js
+++ routes/category.js
@@ -11,23 +11,19 @@ const Permissions = require('../auth/permissions');
  * body of request should be category as json object
  * don't forget 'Content-Type':'application/json'!!
  */
-router.post('/',verifyToken,userController.getUserByAccountId,(req,res, next)=>{
-    const category = req.body;
-    category.accountId = req.userObj.accountId;
-
-    Branch.createCategory(category,(err,cat)=>{
-        if(err){
-            res.status(500).send(err);
-        }
+router.post('/',verifyToken,userController.getUserByAccountId, async (req,res, next)=>{
+    try {
+        const category = req.body;
+        category.accountId = req.userObj.accountId;
+        const cat = await Branch.createCategory(category);
         const userObj = req.userObj;
         userObj.categories.push(cat);
-        Branch.updateUser(userObj.accountId,userObj,(err,usr)=>{
-            if(err){
-                res.status(500).send(err);
-            }
-            res.status(200).send(cat);
-        });
-    });
+        const usr = Branch.updateUser(userObj.accountId, userObj)
+        res.status(200).send(cat);
+    } catch (e) {
+        res.status(500).send({'err':`There was an error creating the category: ${e.message}`})
+    }
+
 });
 
 
@@ -35,14 +31,13 @@ router.post('/',verifyToken,userController.getUserByAccountId,(req,res, next)=>{
 /**
  * Get all categories for user associated with token
  */
-router.get('/',verifyToken,(req,res)=>{
-    Branch.getAllCategories(req.userId, (err,cats)=>{
-        if(err){
-            res.status(500).send({"err":"Can't get categories. Error: \n" + err});
-        }
+router.get('/',verifyToken, async (req,res)=>{
+    try {
+        const cats = Branch.getAllCategories(req.userId)
         res.status(200).send(cats);
-    });
-
+    } catch (e) {
+        res.status(500).send({'err':`There was a problem getting the category: ${err.message}`})
+    }
 });
 
 /**
@@ -50,27 +45,25 @@ router.get('/',verifyToken,(req,res)=>{
  * doesn't match userId of user associated with token
  */
 router.get('/:id', verifyToken, async (req,res)=>{
-    Branch.getCategoryRecursive(req.params.id)
-        .then(
-            (cat)=>{
-                if(!Permissions.checkObjectPermissions(cat.accountId,req.userId)){
-                    res.status(403).send({"err":"You are not authorized to see that category"});
-                };
-                if(cat.err){
-                    res.status(500).send({"err":"Error retrieving category: " + cat.err});
-                }
-                res.status(200).send(cat);
-            }
-        )
+    try {
+        if ((await Branch.verifyOwnership(Branch.Category, req.params.id, req.userId))) {
+            const cat = await Branch.getCategoryRecursive(req.params.id);
+            res.status(200).send(cat);
+        } else {
+            res.status(403).send({"err": "You are not authorized to get that category"});
+        }
+    } catch (e) {
+        res.status(500).send({'err':`There was a problem retrieving that category: ${e.message}`})
+    }
 });
 
 router.delete('/:id', verifyToken, async (req,res)=>{
     try {
-        if (!(await Branch.verifyOwnership(Branch.Category, req.params.id, req.userId))) {
-            res.status(403).send({"err": "You are not authorized to delete that category"});
-        } else {
+        if (await Branch.verifyOwnership(Branch.Category, req.params.id, req.userId)) {
             const deletedCategory = await Branch.deleteCategoryRecursive(req.params.id);
             res.status(200).send(deletedCategory);
+        } else {
+            res.status(403).send({"err": "You are not authorized to delete that category"});
         }
     } catch (e) {
         res.status(500).send({'err':`There was an error deleting the category: ${e.message}`})
@@ -85,32 +78,36 @@ router.delete('/:id', verifyToken, async (req,res)=>{
  * @return deleted task;
  */
 router.delete('/:id/:newParentType/:newParentId', verifyToken, async(req,res)=>{
-   const id = req.params.id;
-    if(!(await Branch.verifyOwnership(Branch.Category, req.params.id, req.userId))){
-        res.status(403).send({"err":"You are not authorized to delete that category"});
-    } else {
-        try {
-            const newParentType = Branch.getParentType(req.params.newParentType);
-            const newParentId = req.params.newParentId;
-            const deletedCategory = await Branch.deleteCategoryAndRebaseChildren(id, newParentType, newParentId);
-            res.status(200).send(deletedCategory);
-        } catch (err) {
-            res.status(500).send('error deleting category' + err.message);
+    const id = req.params.id;
+    try {
+        if(!(await Branch.verifyOwnership(Branch.Category, req.params.id, req.userId))){
+            res.status(403).send({"err":"You are not authorized to delete that category"});
+        } else {
+                const newParentType = Branch.getParentType(req.params.newParentType);
+                const newParentId = req.params.newParentId;
+                const deletedCategory = await Branch.deleteCategoryAndRebaseChildren(id, newParentType, newParentId);
+                res.status(200).send(deletedCategory);
         }
+    } catch (err) {
+        res.status(500).send('error deleting category' + err.message);
     }
 });
 
 router.put('/:id', verifyToken, async (req,res)=>{
     const catId = req.params.id;
     const update = req.body;
-    if(!(await Branch.verifyOwnership(Branch.Category, catId,req.userId))){
-        res.status(403).send({"err":"You are not authorized to modify that Category"})
-    } else {
-        const updatedCategory = await Branch.updateCategory(catId, update);
-        if (updatedCategory.err) {
-            res.status(500).send({"err": "error updating category" + updatedCategory.err});
+    try {
+        if (!(await Branch.verifyOwnership(Branch.Category, catId, req.userId))) {
+            res.status(403).send({"err": "You are not authorized to modify that Category"})
+        } else {
+            const updatedCategory = await Branch.updateCategory(catId, update);
+            if (updatedCategory.err) {
+                res.status(500).send({"err": "error updating category" + updatedCategory.err});
+            }
+            res.status(200).send(updatedCategory);
         }
-        res.status(200).send(updatedCategory);
+    } catch (e){
+        res.status(500).send({'err':`There was a problem modifying that category: ${e.message}`})
     }
 })
 
